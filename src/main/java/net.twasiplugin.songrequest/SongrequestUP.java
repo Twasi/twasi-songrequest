e7f3803f27f08cb3e82f7684489a608057764f4c
Lars Bärtschi <lars.baertschi@bluewin.ch>
2018-07-05 15:31:31 +0200

    Initial commit

--- src/main/java/net.twasiplugin.songrequest/SongrequestUP.java
+++ src/main/java/net.twasiplugin.songrequest/SongrequestUP.java
@@ -1,6 +1,29 @@
 package net.twasiplugin.songrequest;
 
+import net.twasi.core.models.Message.TwasiCommand;
 import net.twasi.core.plugin.api.TwasiUserPlugin;
+import net.twasi.core.plugin.api.events.TwasiCommandEvent;
+import net.twasi.core.plugin.api.events.TwasiInstallEvent;
 
 public class SongrequestUP extends TwasiUserPlugin {
+
+    @Override
+    public void onInstall(TwasiInstallEvent e) {
+        e.getDefaultGroup().addKey("songrequest.user.*");
+        e.getModeratorsGroup().addKey("songrequest.mod.*");
+    }
+
+    @Override
+    public void onUninstall(TwasiInstallEvent e) {
+        e.getDefaultGroup().removeKey("songrequest.user.*");
+        e.getModeratorsGroup().removeKey("songrequest.mod.*");
+    }
+
+    @Override
+    public void onCommand(TwasiCommandEvent e) {
+        TwasiCommand cmd = e.getCommand();
+
+        // Should only listen to registered commands
+        cmd.reply("Command received! [songrequest]");
+    }
 }
